c6b5a425df9021fb977d9447f15ba9b9d4ad36a1
mirielimacedo <mirielimacedo@gmail.com>
2024-05-06 15:22:57 +0800

    create a new component Phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return <div className="Phonetic">{props.phonetic.text}</div>;
+}
--- src/Results.js
+++ src/Results.js
@@ -1,23 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
-  console.log(props.results);
-  if (props.results) {
-    return (
-      <div className="Results">
-        {" "}
-        <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
-      </div>
-    );
-  } else {
+  if (!props.results) {
     return null;
   }
+
+  return (
+    <div className="Results">
+      <h2>{props.results.word}</h2>
+      {props.results.phonetics.map((phonetic, index) => (
+        <Phonetic key={index} phonetic={phonetic} />
+      ))}
+      {props.results.meanings.map((meaning, index) => (
+        <Meaning key={index} meaning={meaning} />
+      ))}
+    </div>
+  );
 }
